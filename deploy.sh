31266752cb78167def7b57342f1dd16d00109750
Frithjof Gressmann <git@nocio.de>
2016-08-06 19:23:55 +0100

    Enable SSL autoconfirm

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ hugo
 source config.sh
 
 # Deploy to production
-lftp sftp://$USER:$PASS@$HOST  -e "ssl:verify-certificate false; mirror  --reverse -e -R $SOURCEFOLDER $TARGETFOLDER --verbose; bye"
+lftp sftp://$USER:$PASS@$HOST  -e "ssl:verify-certificate false; set sftp:auto-confirm yes; mirror  --reverse -e -R $SOURCEFOLDER $TARGETFOLDER --verbose; bye"
