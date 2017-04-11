a51e3b46ff91172d2e370e5116efec0018559310
John Gaida <john.gaida@posteo.de>
2017-04-11 09:52:44 +0200

    Polishing

--- createDraft.py
+++ createDraft.py
@@ -4,15 +4,16 @@ import os
 import sys
 
 
-
 def get_title(file_path):
     title = os.path.splitext(file_path)[0]
     title = title.split('_')[1]
     title = title.replace('-', ' ')
     return title
 
-GALLERY_OPTION="gallery"
-LINK_OPTION="link"
+
+GALLERY_OPTION = "gallery"
+LINK_OPTION = "link"
+
 # read in argument value
 if len(sys.argv) == 2:
     OPTION = sys.argv[1]
@@ -23,7 +24,7 @@ else:
 root = tk.Tk()
 root.withdraw()
 file_paths = filedialog.askopenfilenames()
-#file_paths = ('C:/Users/John/projects/johng/static/images/milan/2017-04-05T123000_VIBRA-Team.jpg',
+# file_paths = ('C:/Users/John/projects/johng/static/images/milan/2017-04-05T123000_VIBRA-Team.jpg',
 #              'C:/Users/John/projects/johng/static/images/milan/2017-04-06T063907_Gepaeck.jpg')
 
 # get date of oldest image
@@ -63,5 +64,5 @@ with open('content/post/italy/draft.md', 'w') as output:
 
         output.write(image_link + "\n\n")
 
-    if OPTION == "gallery":
+    if OPTION == GALLERY_OPTION:
         output.write("{{< /gallery >}}")
